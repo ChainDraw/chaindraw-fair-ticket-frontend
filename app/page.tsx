c45a1964c1a56cc01801d81415e1292aa07447eb
Zzh <3169427572@qq.com>
2024-05-04 10:06:14 +0800

    init dir

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-import { Button } from '@/components/ui/button';
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-
+import Header from "@/components/header/Header";
 export default function Home() {
   return (
     <div>
-      <Button>Click me</Button>
-      <ConnectButton />
+      <Header />
     </div>
   );
 }
--- utils/path.ts
+++
@@ -1,4 +0,0 @@
-export const path = {
-  home: "/",
-  //
-};
