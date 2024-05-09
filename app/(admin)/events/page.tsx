a84387235515222eff6aee5e20a75780ae9c1ce9
pinchao <cpc4253@163.com>
2024-05-09 14:01:49 +0800

    后台添加头部组件

--- app/(admin)/events/page.tsx
+++ app/(admin)/events/page.tsx
@@ -1,6 +1,9 @@
+import AdminHeader from '@/components/admin/header/AdminHeader';
+
 export default function Page() {
   return (
     <div>
+      <AdminHeader />
       <div>events</div>
     </div>
   );
---
+++ components/admin/header/AdminHeader.tsx
@@ -0,0 +1,11 @@
+export default function AdminHeader() {
+  return (
+    <header className="flex items-center h-16 p-4 border-b border-b-[#ddd]">
+      <h1 className="w-72 font-bold">chaindraw</h1>
+      <div className="flex justify-between items-center w-full">
+        <h1 className="font-bold">新建演出</h1>
+        <div>xxx@gmail.com</div>
+      </div>
+    </header>
+  );
+}
