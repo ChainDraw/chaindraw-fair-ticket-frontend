9ce7c2a37e81fd99124ef3309d420d6ec68c3835
pinchao <cpc4253@163.com>
2024-05-09 16:56:02 +0800

    顶部栏使用var变量

--- components/admin/header/admin-header.tsx
+++ components/admin/header/admin-header.tsx
@@ -1,8 +1,8 @@
 export default function AdminHeader() {
   return (
-    <header className="flex items-center h-16 p-4 border-b border-b-[#ddd]">
-      <h1 className="w-72 font-bold">chaindraw</h1>
-      <div className="flex justify-between items-center w-full">
+    <header className="flex items-center h-[--admin-header-height] p-4 border-b border-b-[#ddd]">
+      <div className="w-[--admin-sidebar-width] font-bold">chaindraw</div>
+      <div className="flex-1 flex justify-between items-center w-full">
         <h1 className="font-bold">新建演出</h1>
         <div>xxx@gmail.com</div>
       </div>
