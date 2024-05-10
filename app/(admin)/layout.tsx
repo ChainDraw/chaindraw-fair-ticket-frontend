18c19ed8afd82ecbf5564bd250bf69e4f0c41fee
pinchao <cpc4253@163.com>
2024-05-10 15:07:57 +0800

    fix:admin-main部分宽度自适应

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -14,7 +14,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <AdminHeader />
       <div className="flex h-[--admin-main-height]">
         <AdminNav />
-        <main className="p-4">{children}</main>
+        <main className="flex-1 p-4">{children}</main>
       </div>
     </div>
   );
