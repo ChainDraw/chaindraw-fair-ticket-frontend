3b8aa8cb6d6f8c354add65d0ee726690edc39253
pinchao <cpc4253@163.com>
2024-05-12 18:44:37 +0800

    feat:paths路由表添加

--- utils/paths.ts
+++ utils/paths.ts
@@ -1,15 +1,16 @@
 // api v1
 export const paths = {
   client: {
-    home: "/client",
-    profile: "/client/profile",
-    market: "/client/market",
-    lottery: "/client/lottery",
+    home: '/client',
+    profile: '/client/profile',
+    market: '/client/market',
+    lottery: '/client/lottery',
     ticketInfo: (id: string) => {
       return `client/shows/${id}`;
     },
   },
   admin: {
-    events: "admin/events",
+    events: '/events', // 后台活动列表
+    settings: '/settings', // 后台主办方设置
   },
 };
