20eea725077eec9186ef17c313e92334cdc8e891
cris <15362997297@163.com>
2024-05-23 20:11:06 +0800

    ✨ feat(client): 出售抽屉

--- app/client/profile/collection/page.tsx
+++ app/client/profile/collection/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
 import React from "react";
+import SubmitDrawer from "./components/SubmitDrawer.tsx";
 
 export default function CollectionPage() {
   const collection = {
     title: "Ape",
+    price: 0.00001,
     description: "description",
     club: {
       name: "Mutant Ape Yacht Club",
@@ -15,14 +17,14 @@ export default function CollectionPage() {
   return (
     <div className=" min-h-screen flex flex-col md:py-20  2xl:px-0 bg-black text-white">
       <div className="flex items-center justify-end h-14 px-4 mx-10 bg-slate-800 rounded">
-        <Button className="w-36  bg-sky-500 hover:bg-sky-600 ">Sell</Button>
+        <SubmitDrawer price={collection.price} />
       </div>
 
       <div className=" flex  items-start m-10 bor">
         <div className="w-5/12">
           <img
             className="w-11/12 round-xl object-cover overflow-hidden rounded"
-            src="https://i.seadn.io/s/raw/files/f47c6c9b1e8bff3334913b7104959b60.png?auto=format&dpr=1&w=1000"
+            src="https://i.seadn.io/s/raw/files/9a0a2f7945e3cf75c19879d883b6d786.png?auto=format&dpr=1&w=1000"
             alt=""
           />
         </div>
