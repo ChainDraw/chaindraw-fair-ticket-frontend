d35e3c4ff99784acb76691e3fd4995e6e0eb45a0
pinchao <cpc4253@163.com>
2024-06-01 02:31:40 +0800

    重构ipfs图片上传逻辑

--- app/api/uploadFile/route.ts
+++ app/api/uploadFile/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import axios from 'axios';
 
+// 上传图片文件到pinata
 export async function POST(req: NextRequest) {
   try {
     // 从前端拿到file数据，解析出文件名称
@@ -11,29 +12,13 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ error: 'File not provided' }, { status: 400 });
     }
 
-    const imageName = file.name || 'image.jpg';
-
     data.append('file', file); // 1. 追加文件
-
-    const metadata = JSON.stringify({
-      name: imageName,
-      keyvalues: {
-        event: '演唱会',
-      },
-    });
-    data.append('pinataMetadata', metadata); // 2. 追加元数据
-
-    // const options = JSON.stringify({
-    //   cidVersion: 0,
-    // });
-    // data.append('pinataOptions', options); // 3. 追加选项
 
     // 4. 发送请求
     const response = await axios.post(
       'https://api.pinata.cloud/pinning/pinFileToIPFS',
       data,
       {
-        // maxContentLength: Infinity,
         headers: {
           pinata_api_key: process.env.NEXT_PUBLIC_PINATA_API_KEY,
           pinata_secret_api_key: process.env.NEXT_PUBLIC_PINATA_SECRET_API_KEY,
